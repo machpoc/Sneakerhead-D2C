56dae4733ab6716a63c07f6d775ff75914eab1a1
Nayak, U Akshay <u.akshay.nayak@accenture.com>
2022-07-15 17:40:13 +0530

    changed success page url

--- components/Molecules/Payment/CheckoutForm.js
+++ components/Molecules/Payment/CheckoutForm.js
@@ -56,7 +56,7 @@ export default function CheckoutForm() {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "http://localhost:3000/Checkout/Success",
+        return_url: "https://sneakerhead-d2c.vercel.app/Checkout/Success",
         receipt_email: email,
       },
     });
