108167d920a58b7e5932dc105f84cfffa27a6afd
Haneesh MV <haneeshpld@gmail.com>
2022-07-20 10:17:43 +0530

    minor updates

--- pages/api/getProductCount.js
+++ pages/api/getProductCount.js
@@ -4,7 +4,7 @@ export default function api(req, res) {
   axios
     .get(
       `https://api.us-central1.gcp.commercetools.com/onlinestore-poc/product-projections?limit=${req.query.limit}`,
-      { headers: { Authorization: "Bearer LNLXz9pRjxFLTyfgZHwqNerqyWbFSU5L" } }
+      { headers: { Authorization: "Bearer VZKs6X6qnMI6LT7ajtl1BB5DagNCDmao" } }
     )
 
     .then((data) => {
