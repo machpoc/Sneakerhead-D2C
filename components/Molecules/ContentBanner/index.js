20a716b3ea70ac97935b1a4d6f5987400c40f0c3
Haneesh MV <haneeshpld@gmail.com>
2022-07-21 20:04:30 +0530

    content page added

--- components/Molecules/ContentBanner/index.js
+++ components/Molecules/ContentBanner/index.js
@@ -1,12 +1,17 @@
+import Link from 'next/link';
+import router, { useRouter } from 'next/router';
 import React from 'react';
 import ButtonComponent from '../../Atoms/Button/Button';
 import Grid, { Item } from '../../Atoms/Grid';
 import {StyledHeader,StyledContent,StyledButton} from './ContentBanner.styled'
 const ContentBanner = ({header,description,buttonName}) => {
+
+const router = useRouter()
+
     return (<>
     <Grid marginTop="25px" >
 <Item>
-<Grid column={3} rows={1} justifyContent="center" aligncontent="center" backgroundImage="images/refreshed/saveNature.jpg">
+<Grid column={3} rows={1} justifyContent="center" aligncontent="center" backgroundImage="/images/refreshed/saveNature.jpg">
 
 <Item display="flex">
 <StyledHeader>{header}</StyledHeader>
@@ -16,11 +21,18 @@ const ContentBanner = ({header,description,buttonName}) => {
 
 </Item>
 <Item display="flex">
+<Link href={'/content/save-nature'}
+                         as = {`/content/save-nature`} >
+            <a>
+    
 <StyledButton>
-<ButtonComponent fill="true" bg="#D31424" hoverTextColor="#D31424" hoverBg="#ffffff" >
+{buttonName && <ButtonComponent  fill="true" bg="#D31424" hoverTextColor="#D31424" hoverBg="#ffffff" >
                 {buttonName}
-        </ButtonComponent>
+        </ButtonComponent>}
 </StyledButton>
+
+</a>
+</Link>
 </Item>
 </Grid>
 </Item>
---
+++ pages/content/save-nature.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Grid, { Item } from '../../components/Atoms/Grid';
+import ContactMode from '../../components/Molecules/ContactMode';
+import ContactUs from '../../components/Molecules/ContactUs';
+import Footer from '../../components/Molecules/Footer';
+import Navbar from '../../components/Molecules/NavBar';
+import ContentBannner from '../../components/Molecules/ContentBanner';
+
+
+
+const ContentPage = () => {
+    return ( <>
+         <Navbar />
+        <ContentBannner
+        header="TAKE CARE OF NATURE"
+      />
+
+      <Grid columns={12} marginTop="2rem" >
+<Item colStart={2} colEnd={6}  >
+<span style={{fontFamily: "'Open Sans'", fontStyle: "normal", fontWeight: "700", fontSize: "30px", lineHeight: "41px", color: "#000000"}}>Our goal is to make a positive long-term impact</span>
+</Item>
+<Item colStart={7} colEnd={12}>
+</Item>
+      </Grid>
+
+<Grid columns={12} marginTop="2rem">
+    <Item colStart={2} colEnd={12}>
+        <span style={{fontFamily: "'Open Sans'", fontStyle: "normal", fontWeight: "400", fontSize: "20px", lineHeight: "27px"}}>The most often quoted definition comes from the UN World Commission on Environment and Development: “sustainable development is development that meets the needs of the present without compromising the ability of future generations to meet their own needs.”
+
+In the charter for the UCLA Sustainability Committee, sustainability is defined as: “the integration of environmental health, social equity and economic vitality in order to create thriving, healthy, diverse and resilient communities for this generation and generations to come. The practice of sustainability recognizes how these issues are interconnected and requires a systems approach and an acknowledgement of complexity.”
+
+</span>
+    </Item>
+</Grid>
+
+<Grid columns={12} marginTop="2rem">
+<Item colStart={3} colEnd={6} borderRight="1px solid #000000" margin="0 18% 0 0">
+
+<span style={{fontFamily: "'Open Sans'", fontStyle: "normal", fontWeight: "700", fontSize: "100px", lineHeight: "136px",color:"#D31424"}}>66%</span>
+<div>Landfill waste material dropped</div>
+</Item>
+<Item colStart={6} colEnd={9} borderRight="1px solid #000000" margin="0 18% 0 0">
+<span style={{fontFamily: "'Open Sans'", fontStyle: "normal", fontWeight: "700", fontSize: "100px", lineHeight: "136px",color:"#D31424"}}>72%</span>
+<div>Less energy and water than creating new paper products</div>
+
+</Item>
+<Item colStart={9} colEnd={12}>
+
+
+<span style={{fontFamily: "'Open Sans'", fontStyle: "normal", fontWeight: "700", fontSize: "100px", lineHeight: "136px",color:"#D31424"}}>17</span>
+<div>We save 17 trees per each tonne of paper</div>
+</Item>
+</Grid>
+<Grid marginTop="2rem" marginBottom="16px">
+        <ContactUs />
+      </Grid>
+      <ContactMode />
+
+      <Footer />
+
+    </> );
+}
+ 
+export default ContentPage;
